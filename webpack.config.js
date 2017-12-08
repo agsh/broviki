c828ba5e208f51eb3944a6cd3ca56f3c03fe0e28
Andrew D.Laptev <a.d.laptev@gmail.com>
2017-12-08 17:17:05 +0300

    Babel react preset

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 			, exclude: /node_modules/
 			, loader: 'babel-loader'
 			, options: {
-				presets: ['env']
+				presets: ['env', 'react']
 			}
 		}, {
 			test: /\.less$/
