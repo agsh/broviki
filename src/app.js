1458ca96292b32348b30707126fb86d5525f0321
Andrew D.Laptev <a.d.laptev@gmail.com>
2019-01-04 15:52:01 +0300

    Server with proxy

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const restify = require('restify');
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+}
+
+const server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(6408, () => {
+    console.log('%s listening at %s', server.name, server.url);
+});
