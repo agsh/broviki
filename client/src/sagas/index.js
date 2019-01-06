46de79b656a0197d36502e98ad5ffa491e0f5609
Andrew D.Laptev <a.d.laptev@gmail.com>
2019-01-06 12:52:30 +0300

    all() instead of []

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,11 +1,11 @@
-import { fork } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 
 import { watcherGetDevices as devices } from './devices';
 import { watcherSaga as dogs } from '../sagas.js'
 
 export default function* sagas() {
-  yield [
+  yield all(
     fork(devices),
     fork(dogs)
-  ];
+  );
 }
